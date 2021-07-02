a3fd7a4642929e6686dddd2f87483fd58be07452
Sickenberg Romain <haux49@gmail.com>
2021-07-02 22:38:34 +0200

    Used some bindings and props

--- Pix4DScene/ContentView.swift
+++ Pix4DScene/ContentView.swift
@@ -6,16 +6,73 @@
 //
 
 import SwiftUI
-import UIKit
-import RealityKit
-import AVFoundation
 
-struct ContentView : View {
+struct ContentView: View {
+    @State var isRecording = false
+
     var body: some View {
-        CameraViewController()
-        .background(Color.blue)
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
-        .edgesIgnoringSafeArea(.all)
+        ZStack {
+            VStack {
+                CameraViewController()
+                    .background(Color.white)
+                    .ignoresSafeArea()
+
+                ControlPannel(isRecording: $isRecording)
+            }
+        }
+    }
+}
+
+struct Blur: UIViewRepresentable {
+    var style: UIBlurEffect.Style = .systemMaterial
+    func makeUIView(context: Context) -> UIVisualEffectView {
+        return UIVisualEffectView(effect: UIBlurEffect(style: style))
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: Context) {
+        uiView.effect = UIBlurEffect(style: style)
+    }
+}
+
+struct ControlPannel: View {
+    @State private var angle = 0.0
+    @State private var distance = 0.0
+
+    @Binding var isRecording: Bool
+
+    private func toggleButton() -> Void {
+        self.isRecording = !self.isRecording
+    }
+
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack() {
+                    VStack {
+                        Text("Angle: \(Int(angle))°")
+                        Slider(value: $angle, in: 0...360)
+                    }
+                    .padding(.leading, 10)
+
+                    Button(action: toggleButton, label: {
+                        Label(isRecording ? "Stop" : "Play", systemImage: isRecording ? "stop" : "play")
+                    })
+                    .frame(minWidth: 0, maxWidth: UIScreen.main.bounds.size.width / 2, alignment: .center)
+                    .padding(.vertical, 40)
+                    .background(Blur())
+                    .clipShape(Circle())
+
+                    VStack(alignment: .center, spacing: 12) {
+                        Text("Distance: \(Int(distance)) m")
+                        Slider(value: $distance, in: 0...100)
+                    }
+                    .padding(.trailing, 10)
+                }
+                .padding(.vertical, 20.0)
+                .background(Blur(style: .systemThinMaterial))
+                .cornerRadius(30)
+            }
+            .padding(.horizontal, 10.0)
+        }
     }
 }
 
